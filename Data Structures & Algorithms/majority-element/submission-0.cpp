f37094c8c7f4326cf7dcacca8080b6b9be7a6789
Arnav Joshi <arnav.joshi.108@gmail.com>
2026-09-10 12:26:51 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(int x : nums)
+        {
+            mp[x]++;
+        }
+
+        for(auto& [num, freq] : mp)
+        {
+            if(freq > nums.size()/2)
+            {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+};
